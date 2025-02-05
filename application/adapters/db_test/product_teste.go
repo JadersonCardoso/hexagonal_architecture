f563fd9ec1fb99967f676e4a1494cfe77ec1306a
jaderson.brandao <jaderson.brandao@gmail.com>
2025-02-05 00:01:06 -0300

    Criando testes do métpdp get

--- application/adapters/db_test/product_teste.go
+++ application/adapters/db_test/product_teste.go
@@ -3,6 +3,10 @@ package db
 import (
 	"database/sql"
 	"log"
+	"testing"
+
+	"github.com/jadersoncardoso/codeedu/go-hexagonal/application/adapters/db"
+	"github.com/stretchr/testify/require"
 )
 
 var Db *sql.DB
@@ -34,5 +38,18 @@ func createProduct(db *sql.DB) {
 		log.Fatal(err.Error())
 	}
 	smtp.Exec()
+
+}
+
+func TestProductDb_Get(t *testing.T) {
+	setUp()
+	defer Db.Close()
+
+	productDb := db.NewProductDb(Db)
+	product, err := productDb.Get("abc")
+	require.Nil(t, err)
+	require.Equal(t, "Product Test", product.GetName())
+	require.Equal(t, "0.0", product.GetPrice())
+	require.Equal(t, "Product Test", product.GetStatus())
 
 }
