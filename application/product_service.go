19c9a520a491a3fcf73ee91bf4839401c71d4573
jaderson.brandao <jaderson.brandao@gmail.com>
2025-02-04 22:29:11 -0300

    finalizando a implemenyação do service

--- application/product_service.go
+++ application/product_service.go
@@ -17,13 +17,37 @@ func (s *ProdructService) Create(name string, price float64) (ProductInterface, 
 	product := NewProduct()
 	product.Name = name
 	product.Price = price
-	_, err : product.IsValid()
-	if err != nill {
+	_, err := product.IsValid()
+	if err != nil {
 		return &Product{}, err
 	}
 	result, err := s.Persistence.Save(product)
 	if err != nil {
-		result &Product{}, err
+		return &Product{}, err
+	}
+	return result, nil
+}
+
+func (s *ProdructService) Enable(product ProductInterface) (ProductInterface, error) {
+	err := product.Enable()
+	if err != nil {
+		return &Product{}, err
+	}
+	result, err := s.Persistence.Save(product)
+	if err != nil {
+		return &Product{}, err
+	}
+	return result, nil
+}
+
+func (s *ProdructService) Disable(product ProductInterface) (ProductInterface, error) {
+	err := product.Disable()
+	if err != nil {
+		return &Product{}, err
+	}
+	result, err := s.Persistence.Save(product)
+	if err != nil {
+		return &Product{}, err
 	}
 	return result, nil
 }
